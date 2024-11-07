497b5c8b7b95a972305c8ba6ae7d6782054fd5c9
_ <128780634+HatataMakuna@users.noreply.github.com>
2024-11-07 15:23:44 +0800

    first commit

---
+++ src/main/java/com/springtest/spring_test/SpringTestApplication.java
@@ -0,0 +1,28 @@
+package com.springtest.spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+//	@GetMapping
+//	public String hello() {
+//		return "Hello World!";
+//	}
+
+	// Returns a json code with Hello and World
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello, World");
+	}
+
+}
